78704448509841acc68c0442b17ea5fc9820e57c
Jan Mikeš <j.mikes@me.com>
2023-01-11 22:01:34 +0100

    Use collection for Order::items

--- src/Entity/Order.php
+++ src/Entity/Order.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Liox\Shop\Entity;
 
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Liox\Shop\Doctrine\AddressDoctrineType;
 use Liox\Shop\Value\Address;
@@ -13,21 +14,21 @@ use Ramsey\Uuid\UuidInterface;
 class Order
 {
     /**
-     * @param array<OrderItem> $items
+     * @param Collection<int, OrderItem> $items
      */
     private function __construct(
         #[ORM\Id]
         #[ORM\Column(type: UuidType::NAME, unique: true)]
         public readonly UuidInterface $id,
 
-        #[ORM\OneToMany(mappedBy: 'order', targetEntity: OrderItem::class)]
-        public readonly array $items,
-
         #[ORM\Column(type: AddressDoctrineType::NAME)]
         public readonly Address $shippingAddress,
 
         #[ORM\Column(type: AddressDoctrineType::NAME, nullable: true)]
         public readonly null|Address $invoicingAddress,
+
+        #[ORM\OneToMany(mappedBy: 'order', targetEntity: OrderItem::class)]
+        public readonly Collection $items,
     ) {
     }
 }
