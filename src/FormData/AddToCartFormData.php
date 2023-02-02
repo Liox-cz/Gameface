611cff3c89978e7165c40c93d19d5f86404964db
Jan Mikeš <j.mikes@me.com>
2023-02-02 08:35:23 +0100

    Add validation to data object

--- src/FormData/AddToCartFormData.php
+++ src/FormData/AddToCartFormData.php
@@ -3,7 +3,12 @@ declare(strict_types=1);
 
 namespace Liox\Shop\FormData;
 
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Uuid;
+
 final class AddToCartFormData
 {
+    #[NotBlank]
+    #[Uuid]
     public string $variantId = '';
 }
