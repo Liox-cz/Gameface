7d681613638712252433182f726cb401ab77619c
Jan Mikeš <j.mikes@me.com>
2023-05-25 00:45:21 +0200

    Fix order of services

--- config/services_test.php
+++ config/services_test.php
@@ -10,13 +10,13 @@ return static function(ContainerConfigurator $configurator): void
 {
     $services = $configurator->services();
 
-    // Data fixtures
-    $services->load('Liox\\Shop\\Tests\\DataFixtures\\', __DIR__ . '/../tests/DataFixtures/{*.php}');
-
     $services->defaults()
         ->autoconfigure()
         ->autowire()
         ->public();
 
     $services->alias(CartStorage::class, InMemoryCartStorage::class);
+
+    // Data fixtures
+    $services->load('Liox\\Shop\\Tests\\DataFixtures\\', __DIR__ . '/../tests/DataFixtures/{*.php}');
 };
